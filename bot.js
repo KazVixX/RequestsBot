9f464afef77b275b2dda792c94bdea7f9ec629ac
Gulvan <mitmitmitmit@yandex.ru>
2023-01-18 21:05:58 +0300

    Initial commit

---
+++ bot.js
@@ -0,0 +1,21 @@
+const { Client, Intents } = require('discord.js');
+
+const { clientId, guildId, token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+client.login(token);
